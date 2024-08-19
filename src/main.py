ea475a5776f25a0dd749c1def52ad2b25d165529
Hytalo Bassi <hytalo-bassi@protonmail.com>
2024-08-19 15:20:52 -0400

    feat: improves usability of script

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from os import listdir, getcwd
 import numpy as np
 from PIL import Image
 from os.path import isfile, join
-
-print("Importing brisque... this may take a while")
+import argparse
 from brisque import BRISQUE
+from rich.progress import Progress
 
-brisque = BRISQUE(url = False)
+brisque = BRISQUE(url=False)
 csv_content = ""
 
 
@@ -16,14 +16,23 @@ def get_images_path(dir):
 
 
 def score_images(image_paths, n: float = None):
-    for img_path in image_paths:
-        img = Image.open(img_path)
-        arr = np.asarray(img)
-        scr = brisque.score(img = arr)
-        if n == None:
-            add_score_to_csv(f"{img_path}:{scr}")
-        elif scr <= n:
-            add_score_to_csv(f"{img_path}:{scr}")
+
+    with Progress() as progress:
+        task = progress.add_task('[green]Ranking...', total=len(image_paths))
+
+        for img_path in image_paths:
+            try:
+                img = Image.open(img_path)
+                arr = np.asarray(img)
+                scr = brisque.score(img=arr)
+                if n == None:
+                    add_score_to_csv(f"{img_path}:{scr}")
+                elif scr <= n:
+                    add_score_to_csv(f"{img_path}:{scr}")
+
+                progress.update(task, advance=1)
+            except:
+                progress.update(task, advance=1)
 
 
 def add_score_to_csv(content: str):
@@ -31,28 +40,36 @@ def add_score_to_csv(content: str):
     csv_content += f"{content};"
 
 
-def write_csv(content: str):
-    csv_file = open("stats.csv", 'w')
-    
+def write_csv(content: str, filepath: str):
+    csv_file = open(filepath, "w")
+
     print("Saving stats...")
     csv_file.write(content)
     csv_file.close()
     print("Stats saved!")
 
 
-args = sys.argv
-n = 100
+parser = argparse.ArgumentParser(description="Score photos")
+parser.add_argument(
+    "dir",
+)
+parser.add_argument(
+    "--score",
+    metavar="N",
+    dest="n",
+    action="store_const",
+    help="the maximum score allowed",
+    default=60,
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    default="stats.csv",
+    help="the output to stats",
+)
 
-if len(args) == 1:
-    print(
-            "Incorrect usage! Missing required argument <absolute_directory>\n"
-            "Syntax:\n--------\npython main.py <absolute_directory> [n]\n"
-            "Parameters:\n-----------\nabsolute_directory: This directory \n"
-            "is where the photos are stored\nn: The maximum score allowed score")
-    sys.exit(1)
-elif len(args) == 3:
-    n = args[2]
+namespace = parser.parse_args(sys.argv[1:])
 
-img_paths = get_images_path(args[1])
-score_images(img_paths, n)
-write_csv(csv_content)
+img_paths = get_images_path(namespace.dir)
+score_images(img_paths, int(namespace.n))
+write_csv(csv_content, namespace.output)
